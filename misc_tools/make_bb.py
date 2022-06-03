53639fec690c3f84c823d1e57a533d5a1813b474
Stéphane Ilic <ilic@fzu.cz>
2022-06-03 22:13:45 +0200

    Improved bb making script

--- misc_tools/make_bb.py
+++ misc_tools/make_bb.py
@@ -48,7 +48,6 @@ for s in spe:
     delta_dec = 0.5 * (offs[1].deg.min() + offs[1].deg.max())
     clu_full_data['RA'][s] += delta_ra
     clu_full_data['DEC'][s] += delta_dec
-'''
 
 # All clusters sky coordinates again after fix
 c = SkyCoord(
@@ -56,6 +55,7 @@ c = SkyCoord(
     dec=clu_full_data['DEC']*u.degree,
     frame='icrs',
 )
+'''
 
 # Parameters:
 # - input images resolution in arcsec/pix
@@ -73,21 +73,25 @@ def get_boxes(i):
     # Angular separation of all member galaxies around ith cluster
     seps = c[i].separation(c2)
 
-    # First pruning to keep only galaxies in disk of radius 1.5x the image width
+    # First pruning to keep only galaxies in disk of radius 1.5x the image half-width
+    # (sqrt(2)x the image half-width should be enough, but we keep it safe)
     g = np.where(seps.arcsec < (1.5 * half_width * reso))[0]
 
-    # Spherical offsets of those galaxies around current cluster
+    # Spherical offsets of those galaxies around current cluster (which should be at center)
     offs = c[i].spherical_offsets_to(c2[g])
 
     # Testing if the galaxies are in the image
-    test = (np.abs(offs[0].arcsec) <= (half_width * reso)) & (np.abs(offs[1].arcsec) <= (half_width * reso))
+    test = (
+        (np.abs(offs[0].arcsec) <= (half_width * reso))
+        & (np.abs(offs[1].arcsec) <= (half_width * reso))
+    )
 
-    # Grab ids of clusters to which belong the galaxies inside the image
+    # Grab ids of clusters to which the galaxies inside the image belong
     IDs_clus_inside = np.unique(mem_full_data['ID'][g[test]])
 
     # For each cluster in the image (at least partially), compute:
     # - bounding box coordinates
-    # - area of box inside image
+    # - area of the part of the bounding box which is inside image
     # - if cluster is the main one at the center
     # - (lambda) redshift of cluster
     out = []
@@ -138,24 +142,28 @@ with open(fname, "r") as fp:
 '''
 
 
-############################
-# MAKE BB FILES FOR YOLOV3 #
-############################
+##############################
+# MAKE BBOX FILES FOR YOLOV3 #
+##############################
 
-# Input/output folder and files names
-#root = "/home/users/ilic/ML/ML_clusters_project/YOLOV3/runs/2048x2048_ds4_0p396_pad50_zcut0p3"
-#root = "/home/users/ilic/ML/ML_clusters_project/YOLOV3/runs/2048x2048_ds4_0p396_pad50_zbins"
-# root = "/home/users/ilic/ML/ML_clusters_project/YOLOV3/runs/2048x2048_ds4_0p396_pad50_zbins_ovl"
-# root = "/home/users/ilic/ML/ML_clusters_project/YOLOV3/runs/2048x0p396_ds1_mb32_nopad_A"
+# Input/output root folder
 root = "/home/users/ilic/ML/ML_clusters_project/YOLOV3/runs/"
+# root += 2048x2048_ds4_0p396_pad50_zcut0p3"
+# root += 2048x2048_ds4_0p396_pad50_zbins"
+# root += 2048x2048_ds4_0p396_pad50_zbins_ovl"
+# root += 2048x0p396_ds1_mb32_nopad_A"
 root += "2048x0p396_ds4_mb8_nopad_nocl"
+
+# Set up filenames
 fname_train = root + "/train.txt"
 fname_valid = root + "/valid.txt"
 imgs_path = '/home/users/ilic/ML/SDSS_image_data/redmapper_2048x2048_0p396127'
 # imgs_path = '/home/users/ilic/ML/SDSS_image_data/redmapper_2048x2048_0p792254'
 # imgs_path = '/home/users/ilic/ML/SDSS_image_data/redmapper_2016x2016_0p396127'
 
-# First checks on the main cluster in the image
+# First: loop over all clusters (and associated images) and perform checks on
+# the "main" cluster in the image (the one in the center), and keep only the
+# ones that fulfil those checks
 keep = []
 for i, r in enumerate(res):
     for clus in r:
@@ -168,40 +176,41 @@ for i, r in enumerate(res):
             fH = min(2 * half_width, y2) - max(0, y1)
             # List of conditions to apply:
             conds = [True]
-            # - main cluster has to be fully in image
+            # - main cluster bbox has to be fully in the image
             # conds.append((L == fL) & (H == fH))
             # - main cluster has to be at z>0.3
             #conds.append(clu_full_data['Z_LAMBDA'][ix] > 0.3)
             if all(conds):
                 keep.append(i)
-            # else:
-            #     if not ((L > 2 * half_width) | (H > 2 * half_width)):
-            #         print(i, L, H, fL * fH / L / H)
-            #     else:
-            #         print("Cluster %s is out!" % i)
+
+# Split between training and validation sample
 cut = len(keep) // 2
 
 # Shuffle for good measure
 np.random.seed(42)
 np.random.shuffle(keep)
 
-# For z binning
-# z_bins = np.linspace(0.12, 0.6, 9)
-# z_mid = np.linspace(0.15, 0.6, 10)
+# Useful stuff for z binning
+#z_bins = np.linspace(0.12, 0.6, 9)
+#z_mid = np.linspace(0.15, 0.6, 10)
 
-# For variable regression
-# regvar = clu_full_data['Z_LAMBDA']
-# regvarerr = clu_full_data['Z_LAMBDA_ERR']
-# g = clu_full_data['Z_SPEC'] != -1.
-# regvar[g] = clu_full_data['Z_SPEC'][g]
-# regvarerr[g] = 0.001
-# me = regvar.mean()
-# st = regvar.std()
-# regvar = (regvar - me) / st
-# regvarerr = regvarerr / st
+# Useful for variable regression
+#regvar = clu_full_data['Z_LAMBDA']
+#regvarerr = clu_full_data['Z_LAMBDA_ERR']
+#g = clu_full_data['Z_SPEC'] != -1.
+#regvar[g] = clu_full_data['Z_SPEC'][g]
+#regvarerr[g] = 0.001
+#me = regvar.mean()
+#st = regvar.std()
+#regvar = (regvar - me) / st
+#regvarerr = regvarerr / st
 
 
-# Main loop which writes the training and validation file
+# Main loop which writes the training and validation files;
+# for each image, it makes a list of all bounding boxes present,
+# and performs a few checks to decide whether or not to include them
+# in the training; then, it writes their coordinates (and possibly
+# other variables) in the main training/validation file
 for i, ix in enumerate(keep):
     path = "%s/%s.jpeg" % (imgs_path, clu_full_data['ID'][ix])
     str_to_write = ''
@@ -238,10 +247,12 @@ for i, ix in enumerate(keep):
                 fL * fH / L / H,
             )
     str_to_write += '\n'
+    # Write bboxes in training or validation file according to the decided cut
     with open(fname_train if i < cut else fname_valid, 'a') as f:
         f.write(str_to_write)
 
-# Summary of cluster rules
+# Summary of cluster rules, to be manually adjusted depending
+# on the choice of the previous choices of conditions
 with open(root + "/which_clusters.txt", 'w') as f:
     f.write("Main cluster:\n")
     # f.write("- has to be fully in image\n")
@@ -256,19 +267,29 @@ with open(root + "/which_clusters.txt", 'w') as f:
     # f.write("-  std = %s\n" % st)
 
 
-### Add empty imgs
-import healpy as hp
-from tqdm import tqdm
+#################################################################################
+# Make a validation file full of "empty" (=field) images that are within the    #
+# SDSS footprint and the more restricted sky region choosen by redmapper people #
+#################################################################################
 
+# Healpix required
+import healpy as hp
+
+# How many empty images requested
 n_want = 13056
-#12203
+
+# Output validation file name
 out_fname = "/home/users/ilic/ML/ML_clusters_project/YOLOV3/runs/2048x0p396_ds4_mb8_nopad_nocl/valid_empty.txt"
+
+# Import SDSS/redmapper footprint (in the form of an Healpix map)
 nside = 2048
 m = np.load('poly_map.npz')['arr_0']
 
-# !!! only bad file : '39719858979976774.txt'
-
+# Path to empty/field image folder
+# WARNING!!! there is one bad file/image : '39719858979976774.txt/.jpeg'
 empty_path = "/home/users/ilic/ML/SDSS_image_data/empty_2048x2048_0p396127"
+
+# List all field images positions in the sky and if they are in the footprint
 ra, dec, good, fnames = [], [], [], []
 all_fnames = os.listdir(empty_path)
 nf = len(all_fnames)
@@ -281,6 +302,7 @@ for ix, fn in tqdm(enumerate(all_fnames),total=nf):
         good.append(m[pix])
         fnames.append(fn)
 
+# Write validation file with requested amount of empty images
 ct = 0
 with open(out_fname, "w") as f:
     for g, fn in zip(good, fnames):
@@ -289,104 +311,3 @@ with open(out_fname, "w") as f:
             f.write(empty_path + '/' + fn[:-3] + 'jpeg\n')
         if ct == n_want:
             break
-
-
-
-'''
-#####################################
-########## FOR keras-yolo3 ##########
-#####################################
-
-keep = []
-for i, r in enumerate(res):
-    tmp = np.array(r)
-    g = tmp[:, -1] == 1
-    if tmp[g, -2][0] == 1.:
-        keep.append(i)
-
-from pascal_voc_writer import Writer
-# Writer(path, width, height)
-# writer = Writer('path/to/img.jpg', 800, 400)
-# ::addObject(name, xmin, ymin, xmax, ymax)
-# writer.addObject('cat', 100, 100, 200, 200)
-# ::save(path)
-# writer.save('path/to/img.xml')
-
-# for ix in tqdm(keep, smoothing=0):
-def prep_files(ix):
-    path1 = "/home/users/ilic/ML/SDSS_image_data/redmapper_2048x2048_0p396127/%s.jpeg" % clu_full_data['ID'][ix]
-    path2 = "/home/users/ilic/ML/ML_clusters_project/keras-yolo3/data/imgs/%s.jpeg" % clu_full_data['ID'][ix]
-    os.system('ln -s %s %s' % (path1, path2))
-    writer = Writer(path2, 2048, 2048)
-    for r in res[ix]:
-        if r[-2] >= thres:
-            writer.addObject(
-                'cluster',
-                max(0, r[0]),
-                max(0, r[1]),
-                min(2048, r[2]),
-                min(2048, r[3]),
-            )
-    path3 = "/home/users/ilic/ML/ML_clusters_project/keras-yolo3/data/annots/%s.xml" % clu_full_data['ID'][ix]
-    writer.save(path3)
-    return 1
-if __name__ == '__main__':
-    pool = Pool(32)
-    res = list(
-        tqdm(
-            pool.imap(prep_files, keep),
-            total=len(keep),
-            smoothing=0.,
-        )
-    )
-    pool.close()
-    pool.join()
-
-################################
-########## FOR yolov4 ##########
-################################
-
-keep = []
-for i, r in enumerate(res):
-    tmp = np.array(r)
-    g = tmp[:, -1] == 1
-    if tmp[g, -2][0] == 1.:
-        keep.append(i)
-cut = len(keep) // 2
-
-np.random.seed(42)
-np.random.shuffle(keep)
-
-fname_train = "/home/users/ilic/ML/ML_clusters_project/yolov4/train_2048x2048_0p396_pad50.txt"
-fname_valid = "/home/users/ilic/ML/ML_clusters_project/yolov4/valid_2048x2048_0p396_pad50.txt"
-
-for ix in tqdm(keep[:cut], smoothing=0):
-    path = "/home/users/ilic/ML/SDSS_image_data/redmapper_2048x2048_0p396127/%s.jpeg" % clu_full_data['ID'][ix]
-    str_to_write = ''
-    str_to_write += path
-    for r in res[ix]:
-        if r[-2] >= thres:
-            ctr_x = 0.5 * (min(2048, r[2]) + max(0, r[0])) / 2048.
-            ctr_y = 0.5 * (min(2048, r[3]) + max(0, r[1])) / 2048.
-            w = (min(2048, r[2]) - max(0, r[0])) / 2048.
-            h = (min(2048, r[3]) - max(0, r[1])) / 2048.
-            str_to_write += ' 0,%s,%s,%s,%s' % (ctr_x, ctr_y, w, h)
-    str_to_write += '\n'
-    with open(fname_train, 'a') as f:
-        f.write(str_to_write)
-
-for ix in tqdm(keep[cut:], smoothing=0):
-    path = "/home/users/ilic/ML/SDSS_image_data/redmapper_2048x2048_0p396127/%s.jpeg" % clu_full_data['ID'][ix]
-    str_to_write = ''
-    str_to_write += path
-    for r in res[ix]:
-        if r[-2] >= thres:
-            ctr_x = 0.5 * (min(2048, r[2]) + max(0, r[0])) / 2048.
-            ctr_y = 0.5 * (min(2048, r[3]) + max(0, r[1])) / 2048.
-            w = (min(2048, r[2]) - max(0, r[0])) / 2048.
-            h = (min(2048, r[3]) - max(0, r[1])) / 2048.
-            str_to_write += ' 0,%s,%s,%s,%s' % (ctr_x, ctr_y, w, h)
-    str_to_write += '\n'
-    with open(fname_valid, 'a') as f:
-        f.write(str_to_write)
-'''
