1c46411189ee36f2b15f4072de6f682873e5740b
Stéphane Ilic <ilic@fzu.cz>
2022-06-03 21:46:39 +0200

    Overhaul of the image grabbing script

--- misc_tools/get_imgs.py
+++ misc_tools/get_imgs.py
@@ -22,6 +22,35 @@ clu_full_data = hdul1[1].data
 mem_full_data = hdul2[1].data
 n_clu = len(clu_full_data)
 
+# All clusters sky coordinates
+c = SkyCoord(
+    ra=clu_full_data['RA']*u.degree,
+    dec=clu_full_data['DEC']*u.degree,
+    frame='icrs',
+)
+
+# All member galaxies coordinates
+c2 = SkyCoord(
+    ra=mem_full_data['RA']*u.degree,
+    dec=mem_full_data['DEC']*u.degree,
+    frame='icrs',
+)
+
+# Special cases:
+spe = [11, 69, 73, 165, 313, 4540]
+for s in spe:
+    print("Before: RA=%s, DEC=%s" % (clu_full_data['RA'][s], clu_full_data['DEC'][s]))
+    g = np.where(mem_full_data['ID'] == clu_full_data['ID'][s])
+    offs = c[s].spherical_offsets_to(c2[g])
+    delta_ra = 0.5 * (offs[0].deg.min() + offs[0].deg.max())
+    delta_dec = 0.5 * (offs[1].deg.min() + offs[1].deg.max())
+    clu_full_data['RA'][s] += delta_ra
+    clu_full_data['DEC'][s] += delta_dec
+    print("After: RA=%s, DEC=%s" % (clu_full_data['RA'][s], clu_full_data['DEC'][s]))
+
+#################################################################################
+
+
 # # # # # # # # # # #
 # Get images:
 # - source : SDSS DR16 ImgCutout service
@@ -146,3 +175,78 @@ while ct < 1000:
             else:
                 os.system('rm %s' % path)
 '''
+
+#################################################################################
+#################################################################################
+
+
+# # # # # # # # # # #
+# Get images:
+# - source : SDSS DR16 ImgCutout service
+# - positions : PCCS SZ union clusters
+# - size : 2048x2048
+# - resolution : 0.396127 arcsec/pix (native SDSS)
+# # # # # # # # # # #
+'''
+pathData="/home/users/ilic/ML/SZ_data/"
+hdul = fits.open(pathData+'HFI_PCCS_SZ-union_R2.08.fits')
+clu_full_data = hdul[1].data
+n_clu = len(clu_full_data)
+# for i in tqdm(range(n_clu)[int(sys.argv[1])::32]):
+for i in tqdm(range(n_clu)):
+    com = "http://skyserver.sdss.org/dr16/SkyServerWS/ImgCutout/getjpeg?ra=%s&dec=%s&width=2048&height=2048" % (clu_full_data['RA'][i], clu_full_data['DEC'][i])
+    path = "/home/users/ilic/ML/SDSS_image_data/PSZunion_2048x2048_0p396127/%s.jpeg" % clu_full_data['INDEX'][i]
+    if os.path.isfile(path) & (os.path.getsize(path) != 0):
+        print("%s.jpeg already in new folder" % clu_full_data['INDEX'][i])
+    else:
+        os.system('wget -q "%s" -O %s' % (com, path))
+'''
+
+# # # # # # # # # # #
+# Get images:
+# - source : SDSS DR16 ImgCutout service
+# - positions : MCXC clusters
+# - size : 2048x2048
+# - resolution : 0.396127 arcsec/pix (native SDSS)
+# # # # # # # # # # #
+pathData="/home/users/ilic/ML/other_cats/"
+hdul = fits.open(pathData+'mcxc.fits')
+clu_full_data = hdul[1].data
+n_clu = len(clu_full_data)
+# for i in tqdm(range(n_clu)[int(sys.argv[1])::32]):
+for i in tqdm(range(n_clu)):
+    com = "http://skyserver.sdss.org/dr16/SkyServerWS/ImgCutout/getjpeg?ra=%s&dec=%s&width=2048&height=2048" % (clu_full_data['RAdeg'][i], clu_full_data['DEdeg'][i])
+    path = "/home/users/ilic/ML/SDSS_image_data/MCXC_2048x2048_0p396127/%s.jpeg" % i
+    test1 = os.path.isfile(path)
+    test2 = True
+    if test1:
+        test2 = os.path.getsize(path) != 0
+    if test1 & test2:
+        print("%s.jpeg already in new folder" % i)
+    else:
+        os.system('wget -q "%s" -O %s' % (com, path))
+
+
+# # # # # # # # # # #
+# Get images:
+# - source : SDSS DR16 ImgCutout service
+# - positions : COMPRASS clusters
+# - size : 2048x2048
+# - resolution : 0.396127 arcsec/pix (native SDSS)
+# # # # # # # # # # #
+pathData="/home/users/ilic/ML/other_cats/"
+hdul = fits.open(pathData+'comprass.fits')
+clu_full_data = hdul[1].data
+n_clu = len(clu_full_data)
+# for i in tqdm(range(n_clu)[int(sys.argv[1])::32]):
+for i in tqdm(range(n_clu)):
+    com = "http://skyserver.sdss.org/dr16/SkyServerWS/ImgCutout/getjpeg?ra=%s&dec=%s&width=2048&height=2048" % (clu_full_data['RAJ2000'][i], clu_full_data['DEJ2000'][i])
+    path = "/home/users/ilic/ML/SDSS_image_data/COMPRASS_2048x2048_0p396127/%s.jpeg" % i
+    test1 = os.path.isfile(path)
+    test2 = True
+    if test1:
+        test2 = os.path.getsize(path) != 0
+    if test1 & test2:
+        print("%s.jpeg already in new folder" % i)
+    else:
+        os.system('wget -q "%s" -O %s' % (com, path))
